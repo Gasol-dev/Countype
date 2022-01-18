5d28631870e13d047b8f5485ae5b80f4b16be2f1
Gasol-dev <Gasol-dev@github.com>
2022-01-18 18:34:45 +0300

    Initial commit

---
+++ Countype/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Countype
+//
+//  Created by Alexander Lezya on 18.01.2022.
+//
+
+import UIKit
+import SoulverCore
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+}
